4e1463f685068c2495ea87171ec0ce221824b60a
Shaun Croft <hal@Shauns-MacBook-Pro.local>
2022-10-16 17:06:29 -0400

    added side menu

---
+++ src/lib/components/SideMenu.svelte
@@ -0,0 +1,50 @@
+<script type="ts">
+  import {
+    TransitionRoot,
+    Dialog,
+    TransitionChild,
+    DialogOverlay,
+  } from '@rgossiaux/svelte-headlessui';
+  import { XIcon } from '@rgossiaux/svelte-heroicons/solid';
+
+  export let sideMenuIsOpen: boolean = false;
+</script>
+
+<TransitionRoot as="div" show={sideMenuIsOpen}>
+  <Dialog as="div" class="fixed inset-0 z-40" on:close={() => (sideMenuIsOpen = false)}>
+    <TransitionChild
+      as="div"
+      enter="transition-opacity ease-linear duration-300"
+      enterFrom="opacity-0"
+      enterTo="opacity-100"
+      leave="transition-opacity ease-linear duration-300"
+      leaveFrom="opacity-100"
+      leaveTo="opacity-0"
+    >
+      <DialogOverlay class="fixed inset-0 bg-black bg-opacity-25" />
+    </TransitionChild>
+
+    <TransitionChild
+      as="div"
+      class="max-w-xs md:max-w-md  w-full bg-surface-200 drop-shadow-2xl  flex flex-col overflow-y-auto absolute right-0 h-full z-40"
+      enter="transition ease-in-out duration-300 transform"
+      enterFrom="translate-x-full"
+      enterTo="-translate-x-0"
+      leave="transition ease-in-out duration-300 transform"
+      leaveFrom="translate-x-0"
+      leaveTo="translate-x-full"
+    >
+      <div class="px-4 pt-5 pb-2 flex justify-end">
+        <button
+          type="button"
+          class="-m-2 p-2 rounded-md inline-flex items-center justify-center text-white"
+          on:click={() => (sideMenuIsOpen = false)}
+        >
+          <span class="sr-only">Close menu</span>
+          <XIcon class="h-6 w-6" aria-hidden="true" />
+        </button>
+      </div>
+      SIDE MENU
+    </TransitionChild>
+  </Dialog>
+</TransitionRoot>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,8 @@
 <script type="ts">
+  import SideMenu from '$lib/components/SideMenu.svelte';
   import { flip } from 'svelte/animate';
   import { circIn } from 'svelte/easing';
+  //   import { HamburgerIcon } from '@rgossiaux/svelte-heroicons/solid';
 
   let descriptorArray: string[] = [
     'Web Developer',
@@ -14,33 +16,55 @@
   let currentDescriptorIndex = 0;
 
   $: descriptorLength = descriptorArray.length;
+  let sideMenuIsOpen = false;
 
-  setInterval(() => {
-    if (currentDescriptorIndex < descriptorLength - 1) {
-      currentDescriptorIndex++;
-    } else {
-      currentDescriptorIndex = 0;
-    }
-  }, 3000);
+  //   setInterval(() => {
+  //     if (currentDescriptorIndex < descriptorLength - 1) {
+  //       currentDescriptorIndex++;
+  //     } else {
+  //       currentDescriptorIndex = 0;
+  //     }
+  //   }, 3000);
 </script>
 
-<div class="flex flex-col items-center justify-center h-[100vh]">
-  <img src={imgSrc} alt="" class=" w-4/5 lg:w-1/3 aspect-auto rounded-full" />
-  <div class="w-full text-center flex flex-col">
-    <h1>Hello, my name is Shaun Croft</h1>
-    <div class="text-left flex w-full items-center justify-center self-start">
-      I am a &nbsp;
-      <div class="px-3 flex border-b-2">
-        <div class="whitespace-nowrap">
-          {#each descriptorArray as descriptor, index (descriptor)}
-            <p>
-              {#if index == currentDescriptorIndex}
-                {descriptor}
-              {/if}
-            </p>
-          {/each}
+<SideMenu bind:sideMenuIsOpen />
+<div class="snap-both">
+  <div
+    class="flex flex-col items-center justify-center h-[100vh] snap-center snap-proximity shrink-0"
+  >
+    <button on:click={() => (sideMenuIsOpen = true)} class="fixed self-end top-2"
+      ><svg
+        xmlns="http://www.w3.org/2000/svg"
+        class="h-6 w-6 mt-2 mr-2"
+        viewBox="0 0 20 20"
+        fill="currentColor"
+      >
+        <path
+          fill-rule="evenodd"
+          d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+          clip-rule="evenodd"
+        />
+      </svg></button
+    >
+    <img src={imgSrc} alt="" class=" w-4/5 lg:w-1/3 aspect-auto rounded-full" />
+    <div class="w-full text-center flex flex-col">
+      <h1>Hello, my name is Shaun Croft</h1>
+      <div class="text-left flex w-full items-center justify-center self-start">
+        I am a &nbsp;
+        <div class="px-3 flex border-b-2">
+          <div class="whitespace-nowrap">
+            {#each descriptorArray as descriptor, index (descriptor)}
+              <p>
+                {#if index == currentDescriptorIndex}
+                  {descriptor}
+                {/if}
+              </p>
+            {/each}
+          </div>
         </div>
       </div>
     </div>
   </div>
+  <div class="flex h-[100vh] snap-center w-full items-center justify-center shrink-0">Resume</div>
+  <div class="flex h-[100vh] snap-center w-full items-center justify-center shrink-0">Contact</div>
 </div>
